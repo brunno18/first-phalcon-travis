a78943b52979e1f141caf8657be0fe55f1edceb3
Brunno Moreira <brunnomoreira.silva@gmail.com>
2016-10-31 08:18:37 -0300

    adicionando testes

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,7 +6,7 @@ $loader = new \Phalcon\Loader();
  * We register here the used directories, including the tests one, otherwise the TestCase couldn't be found.
  */
 $loader->registerNamespaces([
-    'Spider\Tests' => dirname(__DIR__) . '/tests'
+    'FirstPhalconTravis\Tests' => dirname(__DIR__) . '/tests'
 ]);
 
 $loader->register();
